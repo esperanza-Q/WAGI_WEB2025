f3e3f3d12305aa2305a3a1aae36712081669fc7f
eunsol <cindychoi03@naver.com>
2026-01-01 21:07:19 +0900

    feat:tag

--- experience/urls.py
+++ experience/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<int:review_id>/edit/', views.review_edit, name='review_edit'),
     path('<int:review_id>/delete/', views.review_delete, name='review_delete'),
     path('image/<int:image_id>/delete/', views.delete_image, name='delete_image'),
+    path("file/<int:file_id>/delete/", views.delete_file, name="delete_file"),
     path('<int:review_id>/comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
     path('<int:review_id>/scrap/', views.toggle_scrap, name='toggle_scrap'),
 ]
